41e6c3c66dda202003333c56c5b583d733c242d2
C-Erastus <chukpozohnt@gmail.com>
2024-05-28 22:22:10 -0400

    working on the Reg class

---
+++ secureDrop/app.py
@@ -0,0 +1,16 @@
+from registration import registration
+
+
+if __name__ == "__main__":
+
+	reg = registration()
+
+	command = input()
+	# check the data base to see if the user is register
+	if not user exists:
+		ret = input("Do you want to register a new user (y/n)?: ")
+		if ret == y:
+			input("Enter Full Name: ")
+			input("Enter Email Address: ")
+			input("Enter Password: ")
+			input("Re-enter Passowrd: ")
