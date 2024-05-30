1cf6afd7bc7d5ab9b88555b05cde2f410488f65b
C-Erastus <chukpozohnt@gmail.com>
2024-05-29 22:41:36 -0400

    reg class almost completed

--- secureDrop/app.py
+++ secureDrop/app.py
@@ -1,16 +1,15 @@
-from registration import registration
+from registration import _registration
 
 
 if __name__ == "__main__":
 
-	reg = registration()
+	reg = _registration()
 
 	command = input()
 	# check the data base to see if the user is register
-	if not user exists:
+	if command == "secureDrop":
 		ret = input("Do you want to register a new user (y/n)?: ")
-		if ret == y:
-			input("Enter Full Name: ")
-			input("Enter Email Address: ")
-			input("Enter Password: ")
-			input("Re-enter Passowrd: ")
+		if ret == "y":
+			reg.get_name()
+			reg.get_email()
+			reg.get_password()
--- secureDrop/registration.py
+++ secureDrop/registration.py
@@ -1,7 +1,28 @@
 
-class registraton:
+class _registration:
+    def __init__(self):
+        self.name = None
+        self.email = None 
+        self.passwd = None 
 
-    @property
+    def get_name(self):
+        self.name = input("Enter Full Name: ")
+
+    def get_email(self):
+        self.email = input("Enter Email Address: ")
+
+    def get_password(self):
+        passwd = input("Enter Password: ")
+        _passwd = input("Re-enter Password: ")
+
+        while (passwd != _passwd):
+            print("Passwords didn't match")
+            passwd = input("Enter Password: ")
+            _passwd = input("Re-enter Password: ")
+        self.passwd = passwd
+        print("\nPasswords Match\nExiting SecureDrop")
+
+    """@property
     def name(self):
         return self._name
     @property
@@ -10,9 +31,12 @@ class registraton:
 
     @name.setter
     def name(self, name):
-        self._name = name
+        self.name = name
 
     @email.setter
-    def email(self, email):
-        self._email = email)
+    def _email(self, email):
+        self._email = email
+
+    def test_func(self):
+        print("Testing_regristration calls")"""
 
