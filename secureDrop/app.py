0ccb149ff57963afb3f3de37de6241d5147c75d1
C-Erastus <chukpozohnt@gmail.com>
2024-07-03 22:26:24 -0400

    minor changes

--- secureDrop/app.py
+++ secureDrop/app.py
@@ -5,12 +5,12 @@ def help():
 	
 	print("add	-> Add a new contact")
 	print("list	->	List all online contacts")
-	print("send	->	Transfer file to contact")	
+	print("send	->	Transfer file to contact")
 
 if __name__ == "__main__":
 
 	reg = _registration()
-    _login = login()
+	_login = login()
 
 	command = input()
 	# check the data base to see if the user is register
@@ -21,9 +21,9 @@ if __name__ == "__main__":
 			reg.get_name()
 			reg.get_email()
 			reg.get_password()
-        elif ret == "n":
-            _login.get_email()
-            _login.get_password()
-            
-            command = input("Type "help" for Commands")
-            
+
+		elif ret == "n":
+			_login.get_email()
+			_login.get_password()
+
+			command = input("Type \""+help+"\" for Commands")
